a3e494a85b881a0bdc3786fe703b069f3f516985
Your Name <excellchriatian12@gmail.com>
2025-09-19 12:48:14 +0700

    Percobaan 3 & 4 Done

---
+++ Jobsheet04/KeretaApi.java
@@ -0,0 +1,32 @@
+package Jobsheet04;
+
+public class KeretaApi {
+    private String nama;
+    private String kelas;
+    private Pegawai masinis;
+    private Pegawai asisten;
+
+    public KeretaApi(String nama, String kelas, Pegawai masinis) {
+        this.nama = nama;
+        this.kelas = kelas;
+        this.masinis = masinis;
+    }
+
+    public KeretaApi(String nama, String kelas, Pegawai masinis, Pegawai asisten) {
+        this.nama = nama;
+        this.kelas = kelas;
+        this.masinis = masinis;
+        this.asisten = asisten;
+    }
+
+    public String info() {
+        String info = "";
+        info += "Nama KA: " + this.nama + "\n";
+        info += "Kelas: " + this.kelas + "\n";
+        info += "Masinis: \n" + this.masinis.info();
+        if (this.asisten != null) {
+            info += "Asisten: \n" + this.asisten.info();
+        }
+        return info;
+    }
+}
---
+++ Jobsheet04/MainGerbong.java
@@ -0,0 +1,12 @@
+package Jobsheet04;
+
+public class MainGerbong {
+    public static void main(String[] args) {
+        Penumpang p = new Penumpang("12345", "Mr. Krab");
+        Gerbong gerbong = new Gerbong("A", 10);
+        gerbong.setPenumpang(p, 1);
+        System.out.println(gerbong.info());
+        Penumpang budi = new Penumpang("67890", "Budi");
+        gerbong.setPenumpang(budi, 1);
+    }
+}
